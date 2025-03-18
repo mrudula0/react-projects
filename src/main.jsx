ff798b85359a43a136fe6119a1dbee5666975a21
mrudula0 <mrudula4442@gmail.com>
2025-03-18 14:13:02 +0530

    setup code with folder

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import { createRoot } from "react-dom/client";
+import "./style.css";
+import App from "./App.jsx";
+import React from "react";
+
+createRoot(document.getElementById("app")).render(<App />);
