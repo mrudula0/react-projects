1d8737ee20ead44767f6e54bdb5ade14f3093f99
mrudula0 <mrudula4442@gmail.com>
2025-03-19 15:21:53 +0530

    created crud operations for todo

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from './pages/navbar'
 import { BrowserRouter } from 'react-router-dom'
 import PageRoutes from './pages/routes'
-
+import './App.css';
 const App = () => {
   return (
     <BrowserRouter>
--- src/projects/todoList/index.jsx
+++ src/projects/todoList/index.jsx
@@ -1,40 +1,94 @@
 import React, { useState } from "react";
+import "./styles.css";
 
 const TodoList = () => {
   let [inp, setInp] = useState("");
   let [newTodo, setNewTodo] = useState([]);
+  let [edit, setEdit] = useState("");
 
   function handleInput(e) {
     setInp(e.target.value);
   }
   function handleAdd() {
-    if (inp !== "") {
+    if (inp.trim()) {
       setNewTodo([...newTodo, { id: Date.now(), text: inp }]);
-      setInp(""); // Clear input field after adding
+      setInp(""); // Clear the input field after adding
     }
   }
-  console.log(newTodo, "14==");
 
+  function handleDel(id) {
+    setNewTodo(newTodo.filter((item) => item.id !== id));
+  }
+
+  function handleUpdateInfo(id) {
+    let newItem = newTodo.find((item) => item.id === id);
+    console.log(newItem, "21");
+    setInp(newItem.text);
+    setEdit(id);
+  }
+
+  function handleSave() {
+    setNewTodo(
+      newTodo.map((itemm) =>
+        itemm.id === edit ? { ...itemm, text: inp } : itemm
+      )
+    );
+    setInp("");
+    setEdit("");
+  }
   return (
-    <div>
-      <input
-        type="text"
-        placeholder="enter todo"
-        value={inp}
-        onChange={handleInput}
-      />
-      <button onClick={handleAdd}>Add</button>
-      <ul>
-        {newTodo.map((item) => {
-          return (
-            <li key={item.id}>
-              {item.text}
+    <div className="main">
+      <h4>Todo-List</h4>
+      <div>
+        <input
+          className="inpp"
+          type="text"
+          placeholder="enter todo"
+          value={inp}
+          onChange={handleInput}
+        />
 
-              <button>delete</button>
-            </li>
-          );
-        })}
-      </ul>
+        <button onClick={edit ? handleSave : handleAdd} className="btn">
+          {edit ? "UPDATE" : "ADD"}
+        </button>
+      </div>
+      <div>
+        {
+          newTodo && newTodo.length > 0 && 
+       
+        <table border={1}>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th>Delete</th>
+              <th>Update</th>
+            </tr>
+
+            {newTodo.map((item) => (
+              <tr key={item.id}>
+                <td>{item.text}</td>
+                <td>
+                  <button
+                    onClick={() => handleDel(item.id)}
+                    className="del-btn"
+                  >
+                    Delete
+                  </button>
+                </td>
+                <td>
+                  <button
+                    onClick={() => handleUpdateInfo(item.id)}
+                    className="update-btn"
+                  >
+                    Update
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+           }
+      </div>
     </div>
   );
 };
